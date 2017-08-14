2e17b3823e5aa8febc40745d5c99a12a76e4df8b
konica <2571583272@qq.com>
2017-08-14 17:57:27 +0800

    更正说法不准确的注释和变量名

--- http-server-page.go
+++ http-server-page.go
@@ -8,36 +8,36 @@ import (
 )
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
-	indexContent, err := httpFilesBox.String("index-tmpl.html")
+	// get file contents as string
+	contents, err := httpFilesBox.String("index-tmpl.html")
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	tmpl, err := template.New("index.html").Parse(indexContent)
+	tmpl, err := template.New("index.html").Parse(contents)
 	baiduUtil.PrintErrIfExist(err)
 	baiduUtil.PrintErrIfExist(tmpl.Execute(w, version))
 }
 
 func loginJs(w http.ResponseWriter, r *http.Request) {
-	// get file contents as string
-	content, err := httpFilesBox.Bytes("js/login.js")
+	byteCntents, err := httpFilesBox.Bytes("js/login.js")
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	w.Write(content)
+	w.Write(byteCntents)
+}
+
+func jquery(w http.ResponseWriter, r *http.Request) {
+	byteCntents, err := httpFilesBox.Bytes("js/jquery.tiny.js")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Write(byteCntents)
 }
 
 func favicon(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Location", "https://www.baidu.com/favicon.ico")
 	http.Error(w, "", 302)
-}
-
-func jquery(w http.ResponseWriter, r *http.Request) {
-	content, err := httpFilesBox.Bytes("js/jquery.tiny.js")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	w.Write(content)
 }
