707c92503b1bab9826f049cd6c0a07e91e6893bd
konica <2571583272@qq.com>
2017-08-15 00:58:55 +0800

    session 支持 优化代码

--- main.go
+++ main.go
@@ -4,34 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"github.com/GeertJohan/go.rice"
-	"net/http/cookiejar"
+	"github.com/astaxie/beego/session"
 	"os"
 )
 
 const (
-	version = "beta v1.0"
+	version = "beta v1.1"
 )
 
 var (
 	port = flag.String("p", "9090", "HTTP server port.")
 	v    = flag.Bool("v", false, "Displays version information.")
 
-	serverTime   string
-	jar          *cookiejar.Jar
-	httpFilesBox *rice.Box
-	err          error
+	serverTime     string
+	httpFilesBox   *rice.Box
+	err            error
+	globalSessions *session.Manager
 )
 
 //go:generate $GOPATH/bin/rice embed-go
 func init() {
 	flag.Parse()
 	if *v {
-		fmt.Printf("百度BDUSS获取工具 %s (Go runtime go1.8.3).\n", version)
-		fmt.Println("Copyright © 2017, iikira.")
+		fmt.Printf("百度BDUSS获取工具 %s Go语言.\n", version)
+		fmt.Println("Copyright (c) 2017, iikira.")
 		os.Exit(0)
 	}
-	fmt.Printf("百度BDUSS获取工具 %s (Go runtime go1.8.3).\n", version)
+	fmt.Printf("百度BDUSS获取工具 %s Go语言.\n", version)
 	fmt.Printf("运行 %s -h 以获取更多帮助.\n\n", os.Args[0])
+}
+
+func init() {
+	globalSessions, _ = session.NewManager("memory", &session.ManagerConfig{
+		CookieName:      "gosessionid",
+		EnableSetCookie: true,
+		Gclifetime:      3600,
+	})
+	go globalSessions.GC()
 }
 
 func main() {
