8c40451e775329ccf27442e0ff681e4d1b926e2e
konica <2571583272@qq.com>
2018-02-09 14:03:17 +0800

    修复手机号不能登录的bug

--- http_request.go
+++ http_request.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/iikira/Baidu-Login/bdcrypto"
-	"github.com/iikira/BaiduPCS-Go/pcsutil"
 	"github.com/iikira/BaiduPCS-Go/requester"
 	"net/http/cookiejar"
 	"regexp"
@@ -56,9 +55,6 @@ func NewBaiduClinet() *BaiduClient {
 // BaiduLogin 发送 百度登录请求
 func (bc *BaiduClient) BaiduLogin(username, password, verifycode, vcodestr string) (lj *LoginJSON) {
 	isPhone := "0"
-	if pcsutil.ChinaPhoneRE.MatchString(username) {
-		isPhone = "1"
-	}
 
 	enpass, err := bdcrypto.RSAEncryptOfWapBaidu(bc.rsaPublicKeyModulus, []byte(password+bc.serverTime))
 	if err != nil {
@@ -68,20 +64,21 @@ func (bc *BaiduClient) BaiduLogin(username, password, verifycode, vcodestr strin
 	}
 
 	post := map[string]string{
-		"username":   username,
-		"password":   enpass,
-		"verifycode": verifycode,
-		"vcodestr":   vcodestr,
-		"isphone":    isPhone,
-		"action":     "login",
-		"uid":        "1516806244773_357",
-		"skin":       "default_v2",
-		"connect":    "0",
-		"dv":         "tk0.408376350146535171516806245342@oov0QqrkqfOuwaCIxUELn3oYlSOI8f51tbnGy-nk3crkqfOuwaCIxUou2iobENoYBf51tb4Gy-nk3cuv0ounk5vrkBynGyvn1QzruvN6z3drLJi6LsdFIe3rkt~4Lyz5ktfn1Qlrk5v5D5fOuwaCIxUobJWOI3~rkt~4Lyi5kBfni0vrk8~n15fOuwaCIxUobJWOI3~rkt~4Lyz5DQfn1oxrk0v5k5eruvN6z3drLneFYeVEmy-nk3c-qq6Cqw3h7CChwvi5-y-rkFizvmEufyr1By4k5bn15e5k0~n18inD0b5D8vn1Tyn1t~nD5~5T__ivmCpA~op5gr-wbFLhyFLnirYsSCIAerYnNOGcfEIlQ6I6VOYJQIvh515f51tf5DBv5-yln15f5DFy5myl5kqf5DFy5myvnktxrkT-5T__Hv0nq5myv5myv4my-nWy-4my~n-yz5myz4Gyx4myv5k0f5Dqirk0ynWyv5iTf5DB~rk0z5Gyv4kTf5DQxrkty5Gy-5iQf51B-rkt~4B__",
-		"getpassUrl": "/passport/getpass?clientfrom=&adapter=0&ssid=&from=&authsite=&bd_page_type=&uid=1501513545973_702&pu=&tpl=wimn&u=https://m.baidu.com/usrprofile%3Fuid%3D1501513545973_702%23logined&type=&bdcm=060d5ffd462309f7e5529822720e0cf3d7cad665&tn=&regist_mode=&login_share_strategy=&subpro=wimn&skin=default_v2&client=&connect=0&smsLoginLink=1&loginLink=&bindToSmsLogin=&overseas=&is_voice_sms=&subpro=wimn&hideSLogin=&forcesetpwd=&regdomestic=",
-		"mobilenum":  "undefined",
-		"servertime": bc.serverTime,
-		// "gid":          "7B3E207-25FD-4DA7-B482-A4039C935C86",
+		"username":     username,
+		"password":     enpass,
+		"verifycode":   verifycode,
+		"vcodestr":     vcodestr,
+		"isphone":      isPhone,
+		"loginmerge":   "1", // 加入这个, 不用判断是否手机号了
+		"action":       "login",
+		"uid":          "1516806244773_357",
+		"skin":         "default_v2",
+		"connect":      "0",
+		"dv":           "tk0.408376350146535171516806245342@oov0QqrkqfOuwaCIxUELn3oYlSOI8f51tbnGy-nk3crkqfOuwaCIxUou2iobENoYBf51tb4Gy-nk3cuv0ounk5vrkBynGyvn1QzruvN6z3drLJi6LsdFIe3rkt~4Lyz5ktfn1Qlrk5v5D5fOuwaCIxUobJWOI3~rkt~4Lyi5kBfni0vrk8~n15fOuwaCIxUobJWOI3~rkt~4Lyz5DQfn1oxrk0v5k5eruvN6z3drLneFYeVEmy-nk3c-qq6Cqw3h7CChwvi5-y-rkFizvmEufyr1By4k5bn15e5k0~n18inD0b5D8vn1Tyn1t~nD5~5T__ivmCpA~op5gr-wbFLhyFLnirYsSCIAerYnNOGcfEIlQ6I6VOYJQIvh515f51tf5DBv5-yln15f5DFy5myl5kqf5DFy5myvnktxrkT-5T__Hv0nq5myv5myv4my-nWy-4my~n-yz5myz4Gyx4myv5k0f5Dqirk0ynWyv5iTf5DB~rk0z5Gyv4kTf5DQxrkty5Gy-5iQf51B-rkt~4B__",
+		"getpassUrl":   "/passport/getpass?clientfrom=&adapter=0&ssid=&from=&authsite=&bd_page_type=&uid=1501513545973_702&pu=&tpl=wimn&u=https://m.baidu.com/usrprofile%3Fuid%3D1501513545973_702%23logined&type=&bdcm=060d5ffd462309f7e5529822720e0cf3d7cad665&tn=&regist_mode=&login_share_strategy=&subpro=wimn&skin=default_v2&client=&connect=0&smsLoginLink=1&loginLink=&bindToSmsLogin=&overseas=&is_voice_sms=&subpro=wimn&hideSLogin=&forcesetpwd=&regdomestic=",
+		"mobilenum":    "undefined",
+		"servertime":   bc.serverTime,
+		"gid":          "DA7C3AE-AF1F-48C0-AF9C-F1882CA37CD5",
 		"logLoginType": "wap_loginTouch",
 		"FP_UID":       "0b58c206c9faa8349576163341ef1321",
 		"traceid":      bc.traceid,
