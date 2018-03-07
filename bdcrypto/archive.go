48c7c58e3e4c444474e9102c0b8987fda85d649a
apple <2571583272@qq.com>
2018-03-07 08:19:17 +0800

    gzip file

--- bdcrypto/archive.go
+++ bdcrypto/archive.go
@@ -3,6 +3,7 @@ package bdcrypto
 import (
 	"compress/gzip"
 	"io"
+	"os"
 )
 
 // GZIPCompress GZIP 压缩
@@ -30,4 +31,47 @@ func GZIPUncompress(src io.Reader, writeTo io.Writer) (err error) {
 	}
 
 	return unReader.Close()
+}
+
+// GZIPCompressFile GZIP 压缩文件
+func GZIPCompressFile(filePath string) (err error) {
+	return gzipCompressFile("en", filePath)
+}
+
+// GZIPUnompressFile GZIP 解压缩文件
+func GZIPUnompressFile(filePath string) (err error) {
+	return gzipCompressFile("de", filePath)
+}
+
+func gzipCompressFile(op, filePath string) (err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+
+	f2, err := os.Create(filePath + ".gzip.tmp")
+	if err != nil {
+		f.Close()
+		return
+	}
+
+	defer f2.Close()
+
+	switch op {
+	case "en":
+		err = GZIPCompress(f, f2)
+	case "de":
+		err = GZIPUncompress(f, f2)
+	default:
+		panic("unknown op" + op)
+	}
+
+	if err != nil {
+		os.Remove(f2.Name())
+		return
+	}
+
+	f.Close()
+	os.Remove(filePath)
+	return os.Rename(filePath+".gzip.tmp", filePath)
 }
